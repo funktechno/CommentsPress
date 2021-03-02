25d640be6b84d906daabe137e249e54b85228f2c
lastlink <theefunk@gmail.com>
2021-03-02 11:48:39 -0500

    more tweaks

---
+++ view/500.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include $GLOBALS['root'] . 'common/head.php'; ?>
+
+<body>
+  <header>
+    <div id="topNav">
+      <figure>
+      <img src="<?php echo $GLOBALS['documentRoot']; ?>images/site/logo.png" alt="Comment Logo">
+      </figure>
+      <div class="toplinks">
+        <span><a href="/accounts/">Welcome test</a> |</span> <a href="/accounts/?action=login" id="folder"><img src="/images/site/account.gif" title="folder" alt="Image of red folder"> My account </a> </div>
+    </div>
+    <nav id="bottomNav">
+      <div>
+        <ul>
+          <li><a class="" href="/index.php" title="View the Acme home page">Home</a></li>
+        </ul>
+      </div>
+    </nav>
+    <?php //include '../common/header.php'; ?>
+  </header>
+
+  <main>
+    <section>
+      <h1>Server Error</h1>
+      <p>Sorry, the server experienced a problem.</p>
+      <?php
+      if (isset($error) && $error != null) {
+        // TODO: production mode to hide in depth errors errors
+        // echo '<p>' . $error . '</p>';
+        echo '<p>Contact Server Admin</p>';
+      }
+      ?>
+      <hr>
+    </section>
+  </main>
+
+  <footer class="text-center">
+    <!-- Footer Text  -->
+    <?php include $GLOBALS['root'] . 'common/footer.php'; ?>
+  </footer>
+</body>
+
+</html>
