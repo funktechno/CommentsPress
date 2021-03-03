2d75bd2f65b4a336367a42bcf6b42ab43c68aa11
lastlink <theefunk@gmail.com>
2021-03-02 21:33:30 -0500

    some mail attempts

--- mail/testMail.php
+++ mail/testMail.php
@@ -6,6 +6,14 @@ require_once './mailConfig.php';
 
 // use wordwrap() if lines are longer than 70 characters
 $msg = wordwrap($mailConfig['Msg'],70);
+// echo json_encode($mailConfig);
+// echo $msg;
 
 // send email
-mail($mailConfig['To'],$mailConfig['Subject'],$mailConfig['Msg']);
+// mail($mailConfig['To'],$mailConfig['Subject'],$mailConfig['Msg']);
+
+mail($mailConfig['To'],
+        "This is the message subject",
+        "This is the message body",
+        "From: sender@example.com" . "\r\n" . "Content-Type: text/plain; charset=utf-8",
+        "-fsender@example.com");
---
+++ mail/testSmtp.php
@@ -0,0 +1,25 @@
+<?php
+ require 'Mail.php';
+ require_once './mailConfig.php';
+
+ // Define basic e-mail parameters:
+ $recipient = $mailConfig['To'];
+ $headers['From'] = $mailConfig['From'];
+ $headers['Reply-to'] = $mailConfig['From'];
+ $headers['To'] = $mailConfig['To'];
+ $headers['Subject'] = $mailConfig['Subject'];
+ $headers['Date'] = date('r');
+//  $headers['Message-Id'] = '<' . uniqid() . '@example.com>';
+ $headers['Content-Type'] = 'text/plain; charset=utf-8';
+ $body = $mailConfig['Msg'];
+
+ // Define SMTP authentication parameters:
+ $smtp_params['host'] = $mailConfig['Host'];
+ $smtp_params['auth'] = true;
+ $smtp_params['username'] = $mailConfig['Username'];
+ $smtp_params['password'] = $mailConfig['Password'];
+ $smtp_params['port'] = $mailConfig['Port']
+
+ // Create a Mail class instance with the above parameters, and then send the message:
+ $message = Mail::factory('smtp', $smtp_params);
+ $message->send($recipient, $headers, $body);
