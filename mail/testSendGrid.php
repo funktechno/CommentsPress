ea1775d716c35413cb8203f0cb25e2ec90d96772
lastlink <theefunk@gmail.com>
2021-03-03 21:05:31 -0500

    working send grid

--- mail/testSendGrid.php
+++ mail/testSendGrid.php
@@ -1,6 +1,8 @@
 <?php
 $curl = curl_init();
 require_once './mailConfig.php';
+$postfields = "{\"personalizations\": [{\"to\": [{\"email\": \"" . $mailConfig['To'] . "\"}]}],\"from\": {\"email\": \"" . $mailConfig['From'] . "\"},\"subject\": \"" . $mailConfig['Subject'] . 'send grid' . "\",\"content\": [{\"type\": \"text/plain\",\"value\": \"" . $mailConfig['Msg'] . "\"}]}";
+// echo json_encode($postfields);
 
 curl_setopt_array($curl, [
     CURLOPT_URL => "https://api.sendgrid.com/v3/mail/send",
@@ -10,7 +12,7 @@ curl_setopt_array($curl, [
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => "{\"personalizations\": [{\"to\": [{\"email\": \"" . $mailConfig['To'] . "\"}]}],\"from\": {\"email\": \"" . $mailConfig['From'] . "\"},\"subject\": \"" . $mailConfig['Subject'] . 'send grid' . "\",\"content\": [{\"type\": \"text/plain\",\"value\": \"" . $mailConfig['Msg'] . "\"}]}",
+    CURLOPT_POSTFIELDS => $postfields,
     CURLOPT_HTTPHEADER => [
         "accept: application/json",
         "authorization: Bearer " . $mailConfig['SendGridKey'],
@@ -21,6 +23,8 @@ curl_setopt_array($curl, [
 
 
 $response = curl_exec($curl);
+$info = curl_getinfo($curl);
+$http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 $err = curl_error($curl);
 
 curl_close($curl);
@@ -28,5 +32,7 @@ curl_close($curl);
 if ($err) {
     echo "cURL Error #:" . $err;
 } else {
+    echo 'Unexpected HTTP code: ', $http_code, "\n";
+    echo json_encode($response);
     echo "sucess send grid email";
 }
