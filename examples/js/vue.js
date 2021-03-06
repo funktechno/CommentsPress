43fe03c54e301c6d3d075818aafb4b449099268c
lastlink <theefunk@gmail.com>
2021-03-05 19:00:28 -0500

    working vue load example

--- examples/js/vue.js
+++ examples/js/vue.js
@@ -12,11 +12,19 @@ var app = new Vue({
         },
         errors: null
     },
+    filters: {
+        date: function (str) {
+            if (!str) { return '(n/a)'; }
+            str = new Date(str);
+            return str.getFullYear() + '-' + ((str.getMonth() < 9) ? '0' : '') + (str.getMonth() + 1) + '-' +
+                ((str.getDate() < 10) ? '0' : '') + str.getDate();
+        }
+    },
     methods: {
         getComments() {
             this.loading = true;
             this.errors = null;
-            this.$http.post("/comments/?action=get",{"slug": "test"}).then((response) => {
+            this.$http.post("/comments/?action=get", { "slug": "test" }).then((response) => {
                 this.loading = false;
                 console.log(response)
                 // this.message = response.data.message;
