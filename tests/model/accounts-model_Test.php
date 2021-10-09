020fd4a057b2545de8d66481e85969a447f4f3aa
lastlink <theefunk@gmail.com>
2021-10-09 15:42:53 -0400

    more model tests

---
+++ tests/model/accounts-model_Test.php
@@ -0,0 +1,42 @@
+<?php
+// run sql files
+require_once 'config/connections.php';
+require_once 'library/functions.php';
+require_once 'model/accounts-model.php';
+
+// use drmonkeyninja\Average;
+use PHPUnit\Framework\TestCase;
+
+class accounts_modelTest extends TestCase
+{
+
+    public function setUp()
+    {
+    }
+
+    public function testgetClient()
+    {
+        $input="admin@me.com";
+        $result = getClient($input);
+        $expected = "adminuser";
+        $this->assertNotEquals(false, $result);
+        $this->assertEquals($expected, $result['displayName']);
+    }
+
+    public function testcheckExistingEmailTrue()
+    {
+        $input="admin@me.com";
+        $result = checkExistingEmail($input);
+        $expected = 1;
+        $this->assertNotEquals(false, $result);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testcheckExistingEmailFalse()
+    {
+        $input="admin2@me.com";
+        $result = checkExistingEmail($input);
+        $expected = 0;
+        $this->assertEquals($expected, $result);
+    }
+}
