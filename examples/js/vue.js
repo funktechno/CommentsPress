7de6f14b48d9dbee6cce0c07fd682684b3a7d6cc
lastlink <theefunk@gmail.com>
2021-10-11 11:21:37 -0400

    pending approval comment

--- examples/js/vue.js
+++ examples/js/vue.js
@@ -5,7 +5,8 @@ Vue.component('comment-thread', {
         'level',
         'is_guest',
         'user_data',
-        "errors"
+        "errors",
+        "moderate_comments"
     ],
     data() {
         return {
@@ -71,34 +72,42 @@ Vue.component('comment-thread', {
         }
     },
     template: `
-    <div :class="level == 0 ? 'root__thread' : 'thread_level_' + level" class="thread thread_theme_light thread_indented"
+<div :class="level == 0 ? 'root__thread' : 'thread_level_' + level" class="thread thread_theme_light thread_indented"
     role="listitem" aria-expanded="true">
     <article id="remark42__comment-01281c83-af78-455e-880b-dabe74fce317"
         :class="level == 0 ? '' :  'comment_level_' + level" class="comment comment_guest comment_theme_light "
         style="">
-        <div class="comment__info"><img class="avatar-icon avatar-icon_theme_light comment__avatar"
-                src="/images/no-image.png" alt=""><span role="button" tabindex="0" class="comment__username"
-                :title="'anonymous_'+ comment.id">{{ comment.displayName }}</span><a
-                class="comment__time">{{comment.updated_at}}</a><a class="comment__link-to-parent" target="_blank"
-                :href="'./comment/'+comment.parentId" aria-label="Go to parent comment" title="Go to parent comment">
-            </a><span class="comment__score"><span class="comment__vote comment__vote_type_up comment__vote_disabled"
-                    aria-disabled="true" role="button" title="Sign in to vote">Vote up</span><span
-                    class="comment__score-value" title="Controversy: 0.00">0</span><span
-                    class="comment__vote comment__vote_type_down comment__vote_disabled" aria-disabled="true"
-                    role="button" title="Sign in to vote">Vote down</span></span></div>
+        <div class="comment__info">
+            <img class="avatar-icon avatar-icon_theme_light comment__avatar" src="/images/no-image.png" alt="">
+            <span role="button" tabindex="0" class="comment__username" :title="'anonymous_'+ comment.id">{{
+                comment.displayName }}</span>
+            <span role="button" v-if="moderate_comments && comment.approved != '1'" class="comment__score_view_negative">Pending Approval</span>
+            <a class="comment__time">{{comment.updated_at}}</a>
+            <a class="comment__link-to-parent" target="_blank" :href="'./comment/'+comment.parentId"
+                aria-label="Go to parent comment" title="Go to parent comment">
+            </a>
+            <span class="comment__score">
+                <span class="comment__vote comment__vote_type_up comment__vote_disabled" aria-disabled="true"
+                    role="button" title="Sign in to vote">Vote up</span>
+                <span class="comment__score-value" title="Controversy: 0.00">0</span>
+                <span class="comment__vote comment__vote_type_down comment__vote_disabled" aria-disabled="true"
+                    role="button" title="Sign in to vote">Vote down</span>
+            </span>
+        </div>
         <div class="comment__body">
             <div v-if="!edit" class="comment__text raw-content raw-content_theme_light" v-html="comment.commentText">
             </div>
             <div v-else class="comment-form__field-wrapper">
-                <textarea id="textarea_1" class="comment-form__field" :readonly="loading" v-model="body" placeholder="Your comment here" spellcheck="true" style="height: 109px;"></textarea>
-                <button class="button button_kind_primary button_size_large comment-form__button" v-on:click="updateComment()" type="submit">Send</button>
+                <textarea id="textarea_1" class="comment-form__field" :readonly="loading" v-model="body"
+                    placeholder="Your comment here" spellcheck="true" style="height: 109px;"></textarea>
+                <button class="button button_kind_primary button_size_large comment-form__button"
+                    v-on:click="updateComment()" type="submit">Send</button>
             </div>
             <div class="comment__actions">
                 <button v-if="user_data" class="button button_kind_link comment__action" type="button" role="button"
                     tabindex="0">Reply</button>
                 <button class="button button_kind_link comment__action" type="button" role="button"
-                    v-if="user_data && comment.userId == user_data.userInfo.id"
-                    v-on:click="toggleEdit()"
+                    v-if="user_data && comment.userId == user_data.userInfo.id" v-on:click="toggleEdit()"
                     tabindex="0">Edit</button>
                 <span v-if="comment.children && comment.children.length > 1" class="comment__controls"><button
                         class="button button_kind_link comment__control" type="button" role="button" tabindex="0"
@@ -107,7 +116,8 @@ Vue.component('comment-thread', {
             </div>
         </div>
     </article>
-    <comment-thread v-if="showChildren" :key="c.id" v-for="c in comment.children" :errors.sync="errors" :user_data="user_data" :is_guest="is_guest" :comment.sync="c" :level='level+1' />
+    <comment-thread v-if="showChildren" :key="c.id" :moderate_comments="moderate_comments" v-for="c in comment.children" :errors.sync="errors"
+        :user_data="user_data" :is_guest="is_guest" :comment.sync="c" :level='level+1' />
     <div class="thread__collapse" role="button" tabindex="0">
         <div></div>
     </div>
